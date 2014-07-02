d7edb52ccbc8b1554c2d764ac5ecf45d7e788853
George Tuli <gtuli6960@sohamcollege.org.uk>
2014-07-02 19:38:56 +0100

    Uploaded homework
    Uploaded playerPos homework - 2nd July 2014.

--- HW/3/playerPos.py
+++ HW/3/playerPos.py
@@ -1,8 +1,20 @@
+# playerPos.py by George Tuli.
+# This program will find a player's position and post it to the in-game chat.
+
+# Import a couple of external libraries so the program can interface with Minecraft.
 import mcpi.minecraft as minecraft 
-import time 
-mc = minecraft.Minecraft.create("localhost") 
- 
-while True: 
- time.sleep(1.0) 
- pos = mc.player.getPos() 
- print pos.x, pos.y, pos.z
+import time
+from mcpi.block import *
+
+# Setup the API.
+mc = minecraft.Minecraft.create("localhost")
+
+# The main loop, which will run indefinitely.
+while True:
+	# Get the player's position (x,y,z) and set it to the variable 'pos'.
+	pos = mc.player.getPos()
+	
+	# Check if the block in front of the player is not air.
+	if pos.x+1 not AIR:
+		mc.setBlocks(pos.x+1, pos.y, pos.z, pos.x+1, pos.y+1, pos.z, AIR)
+	time.sleep(0.1)
