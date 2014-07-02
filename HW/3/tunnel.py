a10e76c12f871941d62424782b70f719247dd91e
George Tuli <gtuli6960@sohamcollege.org.uk>
2014-07-02 19:40:44 +0100

    Uploaded homework
    Uploaded tunnel homework - 2nd July 2014.

--- HW/3/tunnel.py
+++ HW/3/tunnel.py
@@ -1,11 +1,29 @@
+# tunnel.py by George Tuli.
+# This program will allow the player to easily mine through hills and mountains (and snow) without the use of tools.
+
+# Import a couple of external libraries so the program can interface with Minecraft.
 import mcpi.minecraft as minecraft 
-from mcpi.block import * 
- 
-mc = minecraft.Minecraft.create("localhost") 
- 
-x = 38 # vertical 
-y = 0 # height from sea level 
-z = 7.7 # horizontal 
-mc.player.setPos(x, y, z) 
- 
-mc.setBlock(x, y, z, AIR) 
+import time
+from mcpi.block import *
+
+# Setup the API.
+mc = minecraft.Minecraft.create("localhost")
+
+# Set a diamond block for a point of reference.
+pos = mc.player.getPos()
+mc.setBlock(pos.x+1, pos.y+1, pos.z, WOOD_PLANKS)
+
+# Give the player instrutions.
+mc.postToChat("Face the wooden planks, then walk forward.")
+
+time.sleep(2)
+
+# The main loop, which will run indefinitely.
+while True:
+	# Get the player's position (x,y,z) and set it to the variable 'pos'.
+	pos = mc.player.getPos()
+	
+	# Check if the block in front of the player is not air.
+	block = mc.getBlock(pos.x+1, pos.y, pos.z)
+	if block != AIR or block != WATER:
+		mc.setBlocks(pos.x+1, pos.y, pos.z, pos.x+1, pos.y+1, pos.z, AIR)
